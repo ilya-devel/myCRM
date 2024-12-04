2b994c9649ed66230039c2e0d8679dc9094483cb
ilya.devel <ilya.devel@yandex.ru>
2024-12-04 14:56:36 +0500

    add note model v0.1

--- api/models/Note.js
+++ api/models/Note.js
@@ -1,12 +1,39 @@
 const mongoose = require('mongoose')
+const User = require('./User')
+const { required } = require('joi')
 
 const Schema = mongoose.Schema
 const ObjectId = mongoose.ObjectId
 
-const Note = new Schema({
+const note = new Schema({
     id: ObjectId,
-    title: String
+    author: {
+        type: User._id,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    isComplete: {
+        type: Boolean,
+        default: false
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    },
+    updateAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
-module.exports = mongoose.model("User", Note)
+module.exports = mongoose.model("Note", note)
 
